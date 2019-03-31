9e7a21de1286138bf3c85e6e5ef2150a180d86a8
Jeremy Xue <gg831006@gmail.com>
2019-03-31 17:12:26 +0800

    Init project

---
+++ Touch ID and Face ID Logging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Touch ID and Face ID Logging
+//
+//  Created by JeremyXue on 2019/3/31.
+//  Copyright © 2019 JeremyXue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
